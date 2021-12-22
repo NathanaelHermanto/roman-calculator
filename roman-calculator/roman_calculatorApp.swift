09f954ec81add693b1983558624accd782d59003
Nathanael Hermanto <nathanael887@gmail.com>
2021-12-22 19:22:55 +0100

    Initial Commit

---
+++ roman-calculator/roman_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  roman_calculatorApp.swift
+//  roman-calculator
+//
+//  Created by Nathan on 22.12.21.
+//
+
+import SwiftUI
+
+@main
+struct roman_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
